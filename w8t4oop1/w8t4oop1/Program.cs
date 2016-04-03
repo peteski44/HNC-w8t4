995504fb131f7522f58c2dd40e02cf420add70a5
peteski44 <peteski.day1@hotmail.co.uk>
2016-04-03 17:27:28 +0100

    Workshop 8 Task 4 OOP version

---
+++ w8t4oop1/w8t4oop1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace w8t4oop1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double ringInnerIn = 0;
+            double ringOuterIn = 0;
+            
+            Console.WriteLine("Enter the radius of the inside of the ring");
+            ringInnerIn = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the radius of the outside of the ring");
+            ringOuterIn = Convert.ToDouble(Console.ReadLine());
+
+
+            Ring ring1 = new Ring(ringInnerIn, ringOuterIn);
+
+            Console.WriteLine("the area of the inner circle is: {0}", ring1.InnerCircleArea);
+            Console.WriteLine("the area of the outer circle is {0}", ring1.OuterCircleArea);
+            Console.WriteLine("the area of the ring is {0}", ring1.RingArea);
+            
+        }
+    }
+}
